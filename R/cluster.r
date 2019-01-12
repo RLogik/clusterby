ec102498f54875c65f1b0a46a7b7ac659b9521e4
RLogik <r.bitlogik@gmail.com>
2019-01-12 17:46:08 +0100

    debugging allgemein

--- R/cluster.r
+++ R/cluster.r
@@ -3,7 +3,7 @@
 #' This package contains methods, which enables clustering in dataframes. Particularly useful for bio-mathematics, cognitive sciences, etc.
 #'
 #' \code{cluster(df, ...)}
-#' @param df Dataframe to be clustered. Method also possible with vectors.
+#' @param df Tibble/Dataframe to be clustered. Method also possible with vectors.
 #' @param groupby Defaults to \code{c()}. Specificies columns, by which data is to be preliminarily divided into groups, within which the clusters are to be built.
 #' @param by Specifies the column(s) for geometric data, according to which the clusters are to be built.
 #' @param isnear A function. This function operates pairs of entries in the columns with geometric data and returns \code{TRUE}/\code{FALSE} if entries are near. Defaults to a Euclidian metric.
@@ -60,7 +60,7 @@ cluster <- function(data, ...) {
 
 	## Erstellung von Spaltennamen (Klusterspalte + Pufferspalte):
 	cols <- names(data);
-	data <- as.tibble(data);
+	data <- as_tibble(data);
 	chunkname <- 'chunk';
 	i <- 0;
 	while(chunkname %in% c(cols, clustername)) {chunkname <- paste0('chunk', i); i <- i+1;}
@@ -69,35 +69,47 @@ cluster <- function(data, ...) {
 	if(!is.vector(groupby)) groupby <- c();
 	leer <- list();
 	n <- nrow(data);
-	for(i in c(1:n)) leer[[i]] <- c();
-	data[[clustername]] <- leer;
-	tib <- data %>% group_by_at(groupby) %>% nest(.key=chunkname);
-	n <- nrow(tib);
+	for(i in c(1:n)) leer[[i]] <- NA;
+	data[, clustername] <- leer;
+	# data <- add_column(data, !!(clustername) := leer);
+	tib <- data %>% group_by_at(groupby) %>% nest(.key=!!(chunkname));
+	m <- nrow(tib);
 
-	for(i in c(1:n)) {
-		df <- tib[i, chunkname][[1]][[1]];
-		df_ <- df[, c(by, clustername)];
-		n <- nrow(df_);
+	for(i in c(1:m)) {
+		chunk <- tib[i, chunkname][[1]][[1]];
 		edges <- list();
+		n <- nrow(chunk);
+		hasedges <- FALSE;
 		for(i in c(1:n)) {
 			r <- 0;
 			e <- c();
-			x <- df_[i, by];
-			for(j in c((i+1):n)) {
-				y <- df_[j, by];
-				if(near(x, y)) {
-					r <- r +  1;
-					e[r] <- j;
+			if(i < n) {
+				x <- chunk[i, by];
+				for(j in c((i+1):n)) {
+					y <- chunk[j, by];
+					if(isnear(x, y)) {
+						r <- r +  1;
+						e[r] <- j;
+					}
 				}
+			}
+			if(length(e) == 0) {
+				e <- NA;
+			} else {
+				hasedges <- TRUE;
 			}
 			edges[[i]] <- e;
 		}
-		clusters <- generateclasses(edges, min_cluster_size, max_cluster_size);
-		ind <- which(!is.na(clusters));
-		clusters <- clusters[ind];
-		df <- df[ind, ];
-		df[, clustername] <- clusters;
-		tib[i, chunkname][[1]][[1]] <- df;
+		if(hasedges) {
+			clusters <- generateclasses(edges, min_cluster_size, max_cluster_size);
+			ind <- which(!is.na(clusters));
+			clusters <- clusters[ind];
+			chunk <- chunk[ind, ];
+			chunk[, clustername] <- clusters;
+		} else {
+			chunk <- chunk[c(), ];
+		}
+		tib[i, chunkname][[1]][[1]] <- chunk;
 	}
 
 	df <- tib %>% unnest();
@@ -116,12 +128,15 @@ generateclasses <- function(edges, min_sz, max_sz) {
 	getclass <- function(i, ind, sz) {
 		nodes = c();
 		children <- c(i);
-		bool <- TRUE;
 		while(sz > 0) {
 			children_ <- c();
-			for(i in children) children_ <- c(children_, edges[[i]]);
+			for(i in children) {
+				e <- edges[[i]];
+				if(length(e) == 1) if(is.na(e) || is.null(e)) next;
+				children_ <- c(children_, e);
+			}
 			children <- children_;
-			if(length(children) > 0) break;
+			if(length(children) == 0) break;
 			filt <- which(children %in% ind);
 			if(length(filt) >= sz) filt <- filt[c(1:sz)];
 			children <- children[filt];
@@ -146,4 +161,6 @@ generateclasses <- function(edges, min_sz, max_sz) {
 			key <- key + 1;
 		}
 	}
+
+	return(classes);
 };
