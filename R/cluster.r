d978eb0c40bdb886bc502d2cfca9686284e63967
RLogik <r.bitlogik@gmail.com>
2019-01-14 07:48:50 +0100

    Effizienzmaßnahmen für linearen Fall

--- R/cluster.r
+++ R/cluster.r
@@ -6,12 +6,14 @@
 #' @param df Tibble/Dataframe to be clustered. Method also possible with vectors.
 #' @param groupby Defaults to \code{c()}. Specificies columns, by which data is to be preliminarily divided into groups, within which the clusters are to be built.
 #' @param by Specifies the column(s) for geometric data, according to which the clusters are to be built.
-#' @param near A function. This function operates pairs of entries in the columns with geometric data and returns \code{TRUE}/\code{FALSE} if entries are near. Defaults to a Euclidian metric.
-#' @param max.dist Defaults to \code{Inf}. If the default euclidean metric is used for \code{near}, this is the maximum tolerated distance between geometric data.
-#' @param strict Defaults to \code{FALSE}. If the default euclidean metric is used for \code{near}, this sets the proximity to be a strict \code{< dist} or else \code{<= dist}.
+#' @param near A function. This function operates pairs of entries in the columns with geometric data and returns \code{TRUE}/\code{FALSE} if entries are near. Defaults to a Manhattan metric.
+#' @param min.dist Defaults to \code{0}. If the default manhattan metric is used for \code{near}, this is the minimum tolerated distance between geometric data.
+#' @param max.dist Defaults to \code{Inf}. If the default manhattan metric is used for \code{near}, this is the maximum tolerated distance between geometric data.
+#' @param strict Defaults to \code{FALSE}. If the default manhattan metric is used for \code{near}, this sets the proximity to be a strict \code{< dist} or else \code{<= dist}.
 #' @param clustername Defaults to \code{'cluster'}. Running \code{df \%>\% clusterby(...)} returns a data frame, which extends \code{df} by 1 column with this name. This column tags the clusters by a unique index.
 #' @param min.size Defaults to \code{0}. If a cluster has fewer elements as this, it will not be viewed as a cluster.
 #' @param split Defaults to \code{FALSE}. If set to \code{TRUE}, then the output will be group the tibble data by cluster (equivalent to performing \code{\%>\% group_by(...)}).
+#' @param is.linear Defaults to \code{FALSE}. If set to \code{TRUE}, then the geometry is assumed to be linear and endowed with a simple difference-metric. This allows for faster computation.
 #' @export cluster
 #' @examples gene %>% cluster(groupby=c('gene','actve'), by='position', dist=400, strict=TRUE, clustername='tag');
 #' @examples protein3d %>% cluster(groupby='celltype', by=c('x','y','z'), dist=2.5,);
@@ -30,48 +32,54 @@ cluster <- function(data, ...) {
 	min_cluster_size <- INPUTVARS[['min.size']];
 	split <- INPUTVARS[['split']];
 	near <- INPUTVARS[['near']];
+	d_min <- INPUTVARS[['min.dist']];
 	d_max <- INPUTVARS[['max.dist']];
 	strict <- INPUTVARS[['strict']];
+	islinear <- INPUTVARS[['is.linear']];
 
+	if(!is.vector(groupby)) groupby <- c();
 	if(!is.logical(strict)) strict <- FALSE;
 	if(!is.logical(split)) split <- FALSE;
+	if(!is.logical(islinear)) islinear <- FALSE;
+	if(!is.numeric(d_min)) d_min <- Inf;
 	if(!is.numeric(d_max)) d_max <- Inf;
 	if(!is.numeric(min_cluster_size)) min_cluster_size <- 0;
 	if(!is.function(near)) {
-		if(!is.character(near)) near <- 'Euclidean';
-		if(near == 'Manhattan') {
+		if(!is.character(near)) near <- 'Manhattan';
+		if(near == 'Euclidean') {
+			if(strict) {
+				near <- function(x, y) {d <- sqrt(sum((x-y)^2)); return(d_min <= d && d < d_max);};
+			} else {
+				near <- function(x, y) {d <- sqrt(sum((x-y)^2)); return(d_min <= d && d <= d_max);};
+			}
+		} else {#if(near == 'Manhattan') {
 			bool <- FALSE;
 			if(strict) {
-				near <- function(x, y) {return(max(abs(x-y)) < d_max);};
+				near <- function(x, y) {d <- max(abs(x-y)); return(d_min <= d && d < d_max);};
 			} else {
-				near <- function(x, y) {return(max(abs(x-y)) <= d_max);};
-			}
-		} else { ## } else if(near == 'Euclidean') {
-			if(strict) {
-				near <- function(x, y) {return(sqrt(sum((x-y)^2)) < d_max);};
-			} else {
-				near <- function(x, y) {return(sqrt(sum((x-y)^2)) <= d_max);};
+				near <- function(x, y) {d <- max(abs(x-y)); return(d_min <= d && d <= d_max);};
 			}
 		}
 	}
-	if(!is.character(clustername)) clustername <- 'cluster';
 
 
 	## Erstellung von Spaltennamen (Klusterspalte + Pufferspalte):
-	cols <- names(data);
 	data <- tibble::as_tibble(data);
+	cols <- names(data);
+	n <- nrow(data);
+	if(!is.character(clustername)) {
+		clustername <- 'cluster'; i <- 0; while(clustername %in% cols) {clustername <- paste0('cluster', i); i <- i+1;}
+	}
 	chunkname <- 'chunk'; i <- 0; while(chunkname %in% c(cols, clustername)) {chunkname <- paste0('chunk', i); i <- i+1;}
 	edgesname <- 'edges'; i <- 0; while(edgesname %in% c(cols, clustername)) {edgesname <- paste0('edges', i); i <- i+1;}
 
 	## Prägruppierung der Daten:
-	if(!is.vector(groupby)) groupby <- c();
-	leer <- list();
-	n <- nrow(data);
-	for(i in c(1:n)) leer[[i]] <- c(NA);
-	tib <- add_column(data, !!(clustername) := leer, !!(edgesname) := leer);
+	leer <- list(); for(i in c(1:n)) leer[[i]] <- c(NA);
+	tib <- add_column(data, !!(clustername):=leer, !!(edgesname):=leer);
+	if(islinear) tib <- tib[order(by), ];
 
 	# Erzeuge Kanten für Klusterbausteine.
-	tib <- nest(group_by_at(data, groupby), .key=!!(chunkname));
+	tib <- data %>% group_by_at(groupby) %>% nest(.key=!!(chunkname));
 	pos0 <- 0;
 	m <- nrow(tib);
 	for(i in c(1:m)) {
@@ -79,19 +87,31 @@ cluster <- function(data, ...) {
 		n <- nrow(chunk);
 		if(n == 0) next;
 		pts <- list(); for(j in c(1:n)) pts[[j]] <- chunk[j, by];
-		edges <- lapply(c(1:n), function(j) {
-			e <- c();
-			if(j < n) {
-				pt <- pts[[j]];
-				pts_ <- pts[c((j+1):n)];
-				bool <- lapply(pts_, function(y) {
-					return(near(pt, y));
-				});
-				e <- pos0 + j + which(unlist(bool));
-			}
-			if(length(e) == 0) e <- c(NA);
-			return(e);
-		});
+		if(islinear) {
+			edges <- lapply(c(1:n), function(j) {
+				e <- c(NA);
+				if(j < n) {
+					pt <- pts[[j]];
+					pt_ <- pts[[j+1]];
+					if(near(pt, pt_)) e <- c(pos0 + j + 1);
+				}
+				return(e);
+			});
+		} else {
+			edges <- lapply(c(1:n), function(j) {
+				e <- c();
+				if(j < n) {
+					pt <- pts[[j]];
+					pts_ <- pts[c((j+1):n)];
+					bool <- lapply(pts_, function(y) {
+						return(near(pt, y));
+					});
+					e <- pos0 + j + which(unlist(bool));
+				}
+				if(length(e) == 0) e <- c(NA);
+				return(e);
+			});
+		}
 		chunk[[edgesname]] <- edges;
 		tib[i, chunkname][[1]][[1]] <- chunk;
 		pos0 <- pos0 + n;
