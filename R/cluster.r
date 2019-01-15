933425f89e00679aa1d530dfc150cf76075d5570
RLogik <r.bitlogik@gmail.com>
2019-01-15 23:33:52 +0100

    presorting und Defaults sinnvoller

--- R/cluster.r
+++ R/cluster.r
@@ -7,15 +7,16 @@
 #' @param by string vector. Specifies the column(s) for geometric data, according to which the clusters are to be built.
 #' @param filter.by string vector. Defaults to \code{c()}. Specificies columns, by which data is to be preliminarily divided into groups, within which the clusters are to be built.
 #' @param keep string vector. Defaults to \code{c()}. Specificies columns, which should be kept in a summary.
-#' @param near function. This function operates pairs of entries in the columns with geometric data and returns \code{TRUE}/\code{FALSE} if entries are near. Defaults to a Manhattan metric.
+#' @param near symmetric function in two arguments. This function operates pairs of entries in the columns with geometric data and returns \code{TRUE}/\code{FALSE} if entries are near. Defaults to a Manhattan metric.
 #' @param min.dist a real number. Defaults to \code{0}. If the default manhattan metric is used for \code{near}, this is the minimum tolerated distance between geometric data.
 #' @param max.dist a real number. Defaults to \code{Inf}. If the default manhattan metric is used for \code{near}, this is the maximum tolerated distance between geometric data.
 #' @param strict boolean. Defaults to \code{FALSE}. If the default manhattan metric is used for \code{near}, this sets the proximity to be a strict \code{< dist} or else \code{<= dist}.
 #' @param clustername string. Defaults to \code{'cluster'}. Running \code{df \%>\% clusterby(...)} returns a data frame, which extends \code{df} by 1 column with this name. This column tags the clusters by a unique index.
 #' @param min.size a natural number. Defaults to \code{0}. If a cluster has fewer elements as this, it will not be viewed as a cluster.
 #' @param split boolean. Defaults to \code{FALSE}. If set to \code{TRUE}, then the output will be group the tibble data by cluster (equivalent to performing \code{\%>\% group_by(...)}).
-#' @param is.linear boolean. Defaults to \code{FALSE}. If set to \code{TRUE}, then the geometry is assumed to be linear and endowed with a simple difference-metric. This allows for faster computation.
-#' @param is.disjoint boolean. Defaults to \code{FALSE}. If set to \code{TRUE} in combination with \code{is.linear=TRUE}, then the clusters must occupy disjoint intervals.
+#' @param is.linear boolean. Defaults to \code{TRUE}, if \code{length(by)=1}, otherwise to \code{FALSE}. If set to \code{TRUE}, then the geometry is assumed to be linear and endowed with a simple difference-metric. This allows for faster computation.
+#' @param is.disjoint boolean. Defaults to \code{TRUE}, if \code{length(by)=1}, otherwise to \code{FALSE}. If set to \code{TRUE} in combination with \code{is.linear=TRUE}, then the clusters must occupy disjoint intervals.
+#' @param presort boolean. Defaults to \code{TRUE}. If \code{TRUE} and \code{is.linear=TRUE}, then the data will be sorted by the \code{by} column first before the clusters are built. Normally this is desirable, however the option is included to prevent this, by setting \code{presort=FALSE}.
 #' @param summary boolean. Defaults to \code{FALSE}. If set to \code{TRUE} in combination with \code{is.linear=TRUE} and **assuming** the user has presorted the data by the \code{by}-column, then a summary of the clusters as intervalls is provided. This makes most sense, if \code{is.disjoint=TRUE}. This produces the columns \code{filter.by, by, pstart, pend, nstart, nend, n} where \code{pstart}, \code{pend} describes the interval, \code{nstart}, \code{nend} provides the original indices in the input data, and \code{n} is the cluster size (number of points).
 #' @export cluster
 #' @examples gene %>% cluster(by='position', filter.by=c('gene','actve'), min.size=4, max.dist=400, strict=FALSE, is.linear=TRUE, is.disjoint=TRUE, summary=TRUE);
@@ -42,19 +43,25 @@ cluster <- function(data, ...) {
 	strict <- INPUTVARS[['strict']];
 	is_linear <- INPUTVARS[['is.linear']];
 	is_disjoint <- INPUTVARS[['is.disjoint']];
-	is_disjoint <- INPUTVARS[['is.disjoint']];
+	presort <- INPUTVARS[['presort']];
 	do_summary <- INPUTVARS[['summary']];
 
 	if(!is.vector(group_by)) group_by <- c();
 	if(!is.vector(keep)) keep <- c();
-	if(!is.logical(strict)) strict <- FALSE;
-	if(!is.logical(split)) split <- FALSE;
-	if(!is.logical(is_linear)) is_linear <- FALSE;
-	if(!is.logical(is_disjoint)) is_disjoint <- FALSE;
-	if(!is.logical(do_summary) || !is_disjoint) do_summary <- FALSE;
+	if(length(by) == 1) {
+		if(!is.logical(is_linear)) is_linear <- TRUE;
+		if(!is.logical(is_disjoint)) is_disjoint <- TRUE;
+		if(!is.logical(do_summary) || !is_disjoint) do_summary <- FALSE;
+		if(!is.logical(presort)) presort <- TRUE;
+	} else {
+		is_linear <- FALSE;
+		is_disjoint <- FALSE;
+		do_summary <- FALSE;
+		presort <- FALSE;
+	}
+	if(!is.numeric(min_cluster_size)) min_cluster_size <- 0;
 	if(!is.numeric(d_min)) d_min <- 0;
 	if(!is.numeric(d_max)) d_max <- Inf;
-	if(!is.numeric(min_cluster_size)) min_cluster_size <- 0;
 	if(!is.function(near)) {
 		if(!is.character(near)) near <- 'Manhattan';
 		if(near == 'Euclidean') {
@@ -64,7 +71,6 @@ cluster <- function(data, ...) {
 				near <- function(x, y) {d <- sqrt(sum((x-y)^2)); return(d_min <= d && d <= d_max);};
 			}
 		} else {#if(near == 'Manhattan') {
-			bool <- FALSE;
 			if(strict) {
 				near <- function(x, y) {d <- max(abs(x-y)); return(d_min <= d && d < d_max);};
 			} else {
@@ -72,6 +78,8 @@ cluster <- function(data, ...) {
 			}
 		}
 	}
+	if(!is.logical(strict)) strict <- FALSE;
+	if(!is.logical(split) || !do_summary) split <- FALSE;
 
 
 	## Erstellung von Spaltennamen (Klusterspalte + Pufferspalte):
@@ -84,10 +92,11 @@ cluster <- function(data, ...) {
 
 	if(is_linear && is_disjoint) {
 		## Füge Indizes für Gruppennamen und zur Erhaltung der Reihenfolge hinzu:
-		tib <- tib[order(tib[[by]]), ];
+		if(presort) tib <- tib[order(tib[[by]]), ];
 		indexname <- uniquecolumnname('index', c(cols, clustername));
 		tib <- tib %>% add_column(!!(indexname):=c(1:n));
 	}
+
 
 	## Indizes für Präsortierung hinzufügen.
 	groupname <- uniquecolumnname('group', c(cols, clustername));
@@ -96,14 +105,20 @@ cluster <- function(data, ...) {
 	tib <- tib %>% add_column(!!(groupname):=c(1:m));
 	tib <- tib %>% unnest();
 
-	if(is_linear && is_disjoint) {
-		## Reihenfolge wiederherstellen.
-		tib <- tib[order(tib[[indexname]]), ];
-		tib <- tib %>% select(-c(indexname));
 
-		if(do_summary) {
+	if(is_linear) {
+		if(is_disjoint) {
+			## Reihenfolge wiederherstellen.
+			tib <- tib[order(tib[[indexname]]), ] %>% select(-c(indexname));
+		} else if(presort) {
+			tib <- tib[order(tib[[groupname]],tib[[by]]), ];
+		} else {
+			tib <- tib[order(tib[[groupname]]), ];
+		}
+
+		if(is_disjoint && do_summary) {
 			oldcols <- unique(c(group_by, keep));
-			summcols <- c('pstart','pend','nstart','nend','n');
+			summcols <- c('pstart','pend','nstart','nend','distance','n');
 			strich <- '';
 			pref <- function(x) {return(paste0(strich,x));};
 			if(max(summcols %in% oldcols)) {
@@ -125,9 +140,9 @@ cluster <- function(data, ...) {
 			g <- tib[i0, groupname][[1]][[1]]
 			pt_ <- pt;
 			while(i1 <= n) {
-				pt__ <- tib[i1, by][[1]][[1]];
 				g_ <- tib[i1, groupname][[1]][[1]]
-				if(!near(pt, pt__) || !(g == g_)) break;
+				pt__ <- tib[i1, by][[1]][[1]];
+				if(!(g == g_) || !near(pt, pt__)) break;
 				pt_ <- pt__;
 				i1 <- i1 +  1;
 			}
@@ -138,7 +153,7 @@ cluster <- function(data, ...) {
 				if(do_summary) {
 					cl <- cl + 1;
 					for(col in oldcols) tib_summ[cl, col] <- tib[i0, col];
-					tib_summ[cl, summcols] <- c(pt, pt_, i0, i1, sz);
+					tib_summ[cl, summcols] <- c(pt, pt_, i0, i1, abs(pt_-pt) + 1, sz);
 				} else {
 					dsel <- c(i0:i1);
 					sel <- c(sel, dsel);
@@ -154,55 +169,12 @@ cluster <- function(data, ...) {
 		} else {
 			data <- tib[sel, ] %>% select(-c(groupname));
 		}
-
-	} else if(is_linear) {
-		tib <- tib[order(tib[[group_by]],tib[[by]]), ];
-		tib <- tib %>% group_by_at(groupname) %>% nest(.key=!!(chunkname));
-
-		m <- nrow(tib);
-		cl <- 0;
-		sel <- c();
-		for(i in c(1:m)) {
-			chunk <- tib[i, chunkname][[1]][[1]];
-			n <- nrow(chunk);
-			if(n < min_cluster_size) next;
-
-			i0 <- 1;
-			sel_ <- c();
-			while(i0 <= n) {
-				i1 <- i0 + 1;
-				pt <- chunk[i0, by][[1]][[1]];
-				pt_ <- pt;
-				while(i1 <= n) {
-					pt__ <- chunk[i1, by][[1]][[1]];
-					if(!near(pt, pt__)) break;
-					pt_ <- pt__;
-					i1 <- i1 +  1;
-				}
-				i1 <- i1 - 1;
-
-				sz <- i1-i0+1;
-				if(sz >= min_cluster_size) {
-					dsel <- c(i0:i1);
-					sel_ <- c(sel_, dsel);
-					chunk[dsel, clustername] <- cl;
-					cl <- cl + 1;
-				}
-				i0 <- i1 + 1;
-			}
-
-			tib[i, chunkname][[1]][[1]] <- chunk[sel_, ];
-			sel <- c(sel, i);
-		}
-
-		data <- tib[sel, ] %>% unnest();
 	} else {
 		edgesname <- uniquecolumnname('edges', c(cols, clustername));
 
 		## Prägruppierung der Daten:
 		leer <- list(); for(i in c(1:n)) leer[[i]] <- c(NA);
 		tib <- tib %>% add_column(!!(clustername):=leer, !!(edgesname):=leer);
-		if(is_linear) tib <- tib[order(tib[[by]]), ];
 
 		# Erzeuge Kanten für Klusterbausteine.
 		tib <- tib %>% group_by_at(groupname) %>% nest(.key=!!(chunkname));
@@ -236,11 +208,12 @@ cluster <- function(data, ...) {
 		clusters <- generateclasses(tib[[edgesname]], min_cluster_size);
 		ind <- which(!is.na(clusters));
 		clusters <- clusters[ind];
-		tib <- tib[ind, ] %>% select(-c(edgesname));
-		data <- tib %>% add_column(!!(clustername) := clusters);
+		data <- tib[ind, ] %>%
+			select(-c(groupname, edgesname)) %>%
+			add_column(!!(clustername) := clusters);
 	}
 
-	if(split && !do_summary) data <- group_by_at(data, c(group_by, clustername)); #%>% nest(.key=!!(dataname);
+	if(split) data <- group_by_at(data, c(group_by, clustername)); #%>% nest(.key=!!(dataname);
 
 	return(data);
 };
